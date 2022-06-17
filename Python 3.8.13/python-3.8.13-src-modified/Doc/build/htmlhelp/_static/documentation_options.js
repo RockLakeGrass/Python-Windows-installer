8dc5cd1be8562460ea740291addc4b98b05551b1
Eric-WangHanming <68141745+Eric-WangHanming@users.noreply.github.com>
2022-06-17 15:42:32 +0800

    Add arm64 support for Python 3.8.13

---
+++ Python 3.8.13/python-3.8.13-src-modified/Doc/build/htmlhelp/_static/documentation_options.js
@@ -0,0 +1,15 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '3.8.13',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SEARCH_LANGUAGE_STOP_WORDS: 
+};
+
+
+
+
